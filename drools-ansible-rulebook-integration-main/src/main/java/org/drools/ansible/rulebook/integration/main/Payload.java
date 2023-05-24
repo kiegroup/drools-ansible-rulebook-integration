ffc2c61a5d17a74e18ea603f435ffc89b804d5cb
mariofusco <mario.fusco@gmail.com>
2023-05-24 17:34:27 +0200

    make rule evaluation atomic
    (cherry picked from commit b00556e10f112688bc0568e7ae8c8a8cad622125)

--- drools-ansible-rulebook-integration-main/src/main/java/org/drools/ansible/rulebook/integration/main/Payload.java
+++ drools-ansible-rulebook-integration-main/src/main/java/org/drools/ansible/rulebook/integration/main/Payload.java
@@ -1,12 +1,17 @@
 package org.drools.ansible.rulebook.integration.main;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.drools.ansible.rulebook.integration.api.ObjectMapperFactory;
 import org.drools.ansible.rulebook.integration.core.jpy.AstRulesEngine;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Payload {
 
@@ -82,6 +87,12 @@ public class Payload {
         return new PayloadRunner(this, engine, sessionId);
     }
 
+    public List<Map> execute(AstRulesEngine engine, long sessionId) {
+        PayloadRunner payloadRunner = new PayloadRunner(this, engine, sessionId);
+        payloadRunner.run();
+        return payloadRunner.getReturnedMatches();
+    }
+
     private static class PayloadRunner implements Runnable {
 
         private final Payload payload;
@@ -89,6 +100,8 @@ public class Payload {
         private final AstRulesEngine engine;
 
         private final long sessionId;
+
+        private List<Map> returnedMatches = new ArrayList<>();
 
         private PayloadRunner(Payload payload, AstRulesEngine engine, long sessionId) {
             this.payload = payload;
@@ -101,7 +114,13 @@ public class Payload {
             long start = System.currentTimeMillis();
             for (int i = 0; i < payload.loopCount; i++) {
                 for (String p : payload.list) {
-                    engine.assertEvent(sessionId, p);
+                    String resultJson = engine.assertEvent(sessionId, p);
+                    ObjectMapper mapper = ObjectMapperFactory.createMapper(new JsonFactory());
+                    try {
+                        returnedMatches.addAll(mapper.readValue(resultJson, List.class));
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
                     sleepSeconds(payload.delay);
                 }
                 sleepSeconds(payload.loopDelay);
@@ -122,6 +141,10 @@ public class Payload {
                     throw new RuntimeException(e);
                 }
             }
+        }
+
+        public List<Map> getReturnedMatches() {
+            return returnedMatches;
         }
     }
 }
