afd37ab1d4715e6fa9547ca5ebcf82ad01356047
Mario Fusco <mario.fusco@gmail.com>
2023-06-06 08:59:16 +0200

    add integration test for once_after (#45)
    (cherry picked from commit 384db0e56acf05e04e77f60275aa23ad843ce9f1)

--- drools-ansible-rulebook-integration-main/src/test/java/org/drools/ansible/rulebook/integration/main/PerfTest.java
+++ drools-ansible-rulebook-integration-main/src/test/java/org/drools/ansible/rulebook/integration/main/PerfTest.java
@@ -7,9 +7,18 @@ import static org.junit.Assert.assertTrue;
 public class PerfTest {
 
     @Test
-    public void test() {
-        long duration = Main.execute("100k_event_rules_ast.json");
+    public void testManyEvents() {
+        checkDuration("100k_event_rules_ast.json", 10_000);
+    }
+
+    @Test
+    public void testOnceAfter() {
+        checkDuration("56_once_after.json", 15_000);
+    }
+
+    private static void checkDuration(String jsonFile, int expectedMaxDuration) {
+        long duration = Main.execute(jsonFile);
         System.out.println("Executed in " + duration + " msecs");
-        assertTrue("There is a performance issue, this test took too long: " + duration + " msecs", duration < 10_000);
+        assertTrue("There is a performance issue, this test took too long: " + duration + " msecs", duration < expectedMaxDuration);
     }
 }
