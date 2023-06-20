b1cf0d77372579a46620d1458511cbc526bd56d9
tarilabs <matteo.mortari@gmail.com>
2023-06-20 07:56:23 +0200

    cleanup RulesModelUtil

--- drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/rulesmodel/RulesModelUtil.java
+++ drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/rulesmodel/RulesModelUtil.java
@@ -22,36 +22,15 @@ public class RulesModelUtil {
 
     public static Fact mapToFact(Map<String, Object> factMap, boolean event) {
         Fact fact = event ? createMapBasedEvent( getPrototype(DEFAULT_PROTOTYPE_NAME), factMap ) : createMapBasedFact( getPrototype(DEFAULT_PROTOTYPE_NAME), factMap );
-        // the culprit, disabled:
-        // populateFact(fact, factMap, "");
-        // fact.set(ORIGINAL_MAP_FIELD, factMap);
         return fact;
     }
-
-    // private static void populateFact(Fact fact, Map<?, ?> value, String fieldName) {
-    //     for (Map.Entry<?, ?> entry : value.entrySet()) {
-    //         String key = fieldName + entry.getKey();
-    //         fact.set(key, entry.getValue());
-    //         if (entry.getValue() instanceof Map) {
-    //             populateFact(fact, (Map<?, ?>) entry.getValue(), key + ".");
-    //         }
-    //     }
-    // }
 
     public static Object factToMap(Object fact) {
         if (fact instanceof Fact) {
-            return (((Fact) fact).asMap());
-        }
-        if (fact instanceof Map) {
-            return (((Map) fact));
+            return ((Fact) fact).asMap();
         }
         return fact;
     }
-
-    // private static Map<String, Object> factToMap(Map<String, Object> factMap) {
-    //     Map<String, Object> map = (Map<String, Object>) factMap.get(ORIGINAL_MAP_FIELD);
-    //     return map != null ? map : factMap;
-    // }
 
     public static Map<String, Object> asFactMap(String json) {
         return new JSONObject(json).toMap();
