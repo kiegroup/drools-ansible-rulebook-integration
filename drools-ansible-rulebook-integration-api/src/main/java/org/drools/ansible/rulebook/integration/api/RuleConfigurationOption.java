826c4a21ec534e8d8e9d5e12d2938dab16a90b87
Matteo Mortari <matteo.mortari@gmail.com>
2023-08-01 08:36:23 +0200

    Introduce fully manual pseudoclock opt (no auto advance) (#70)
    When the option is set, there is no automatic advance of the internal pseudoclock.
    This option is intended for use in testing
    (avoid during debug session for the pseudoclock to diverge
    because of the default automatic advancements)
    
    (cherry picked from commit e21a53d42d7a6d37fd72f11c028d1a24c7480076)

--- drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/RuleConfigurationOption.java
+++ drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/RuleConfigurationOption.java
@@ -3,5 +3,12 @@ package org.drools.ansible.rulebook.integration.api;
 public enum RuleConfigurationOption {
     EVENTS_PROCESSING,
     USE_PSEUDO_CLOCK,
+    /**
+     * When this option is set, there is no automatic advance of the internal pseudoclock.
+     * This option is intended for use in testing
+     * (avoid during debug session for the pseudoclock to diverge
+     * because of the default automatic advancements)
+     */
+    FULLY_MANUAL_PSEUDOCLOCK,
     ASYNC_EVALUATION
 }
