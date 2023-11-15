5badd7c15e6f90bab04895fe1d94085c296d0adb
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2023-11-15 17:10:43 +0900

    - check memory every N events to avoid performance overhead

--- drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/rulesengine/MemoryMonitorUtil.java
+++ drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/rulesengine/MemoryMonitorUtil.java
@@ -1,5 +1,7 @@
 package org.drools.ansible.rulebook.integration.api.rulesengine;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,18 +10,32 @@ public class MemoryMonitorUtil {
     private static final Logger LOG = LoggerFactory.getLogger(MemoryMonitorUtil.class.getName());
 
     public static final String MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD_PROPERTY = "drools.memory.occupation.percentage.threshold";
-
     private static final int DEFAULT_MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD = 90;
-
     private static final int MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD;
 
+    // check memory per configured number of events are consumed
+    public static final String MEMORY_CHECK_EVENT_COUNT_THRESHOLD_PROPERTY = "drools.memory.check.event.count.threshold";
+    private static final int DEFAULT_MEMORY_CHECK_EVENT_COUNT_THRESHOLD = 100;
+    private static final int MEMORY_CHECK_EVENT_COUNT_THRESHOLD;
+
+    private static AtomicInteger eventCount = new AtomicInteger(0);
+
     static {
-        String envValue = System.getenv("DROOLS_MEMORY_THRESHOLD");
-        if (envValue != null && !envValue.isEmpty()) {
+        String memoryThresholdEnvValue = System.getenv("DROOLS_MEMORY_THRESHOLD");
+        if (memoryThresholdEnvValue != null && !memoryThresholdEnvValue.isEmpty()) {
             // Environment variable takes precedence over system property
-            System.setProperty(MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD_PROPERTY, envValue);
+            System.setProperty(MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD_PROPERTY, memoryThresholdEnvValue);
         }
         MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD = Integer.getInteger(MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD_PROPERTY, DEFAULT_MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD); // percentage
+        LOG.info("Memory occupation threshold set to {}%", MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD);
+
+        String eventCountThresholdEnvValue = System.getenv("DROOLS_MEMORY_CHECK_EVENT_COUNT_THRESHOLD");
+        if (eventCountThresholdEnvValue != null && !eventCountThresholdEnvValue.isEmpty()) {
+            // Environment variable takes precedence over system property
+            System.setProperty(MEMORY_CHECK_EVENT_COUNT_THRESHOLD_PROPERTY, eventCountThresholdEnvValue);
+        }
+        MEMORY_CHECK_EVENT_COUNT_THRESHOLD = Integer.getInteger(MEMORY_CHECK_EVENT_COUNT_THRESHOLD_PROPERTY, DEFAULT_MEMORY_CHECK_EVENT_COUNT_THRESHOLD); // number of events
+        LOG.info("Memory check event count threshold set to {}", MEMORY_CHECK_EVENT_COUNT_THRESHOLD);
     }
 
     private MemoryMonitorUtil() {
@@ -27,10 +43,18 @@ public class MemoryMonitorUtil {
     }
 
     public static void checkMemoryOccupation() {
+        // check memory every N events to avoid performance overhead
+        if (eventCount.incrementAndGet() >= MEMORY_CHECK_EVENT_COUNT_THRESHOLD) {
+            doCheckMemoryOccupation();
+            eventCount.set(0);
+        }
+    }
+
+    private static void doCheckMemoryOccupation() {
         int memoryOccupationPercentage = getMemoryOccupationPercentage();
         if (memoryOccupationPercentage > MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD) {
+            // give GC a chance to free some memory
             System.gc(); // NOSONAR
-            // double check to avoid frequent GC
             memoryOccupationPercentage = getMemoryOccupationPercentage();
             if (memoryOccupationPercentage > MEMORY_OCCUPATION_PERCENTAGE_THRESHOLD) {
                 LOG.error("Memory occupation is above the threshold: {}% > {}%. MaxMemory = {}, UsedMemory = {}",
