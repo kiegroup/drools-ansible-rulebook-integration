d60d818b76480e4df3114d33e0fbe4e9e7c0a4d6
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-09-13 16:02:46 +0900

    [1.0.x] Backport isNotDefined issue (#109)
    * Adding isNotDefined test (#105)
    
    * Clarify IsNotDefined operator behaviour when used alone (#106)
    
    * Fix evaluation of is not defined expression (#108)
    
    ---------
    
    Co-authored-by: Mario Fusco <mario.fusco@gmail.com>

--- drools-ansible-rulebook-integration-api/src/test/java/org/drools/ansible/rulebook/integration/api/ProcessEventTest.java
+++ drools-ansible-rulebook-integration-api/src/test/java/org/drools/ansible/rulebook/integration/api/ProcessEventTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.drools.base.reteoo.InitialFactImpl;
 import org.drools.model.PrototypeFact;
 import org.junit.Test;
 import org.kie.api.runtime.rule.Match;
@@ -131,4 +132,164 @@ public class ProcessEventTest {
 
         rulesExecutor.dispose();
     }
-}
+
+    public static final String JSON_IS_NOT_DEFINED =
+            """
+            {
+                "rules": [
+                        {
+                            "Rule": {
+                                "name": "r1",
+                                "condition": {
+                                    "AllCondition": [
+                                        {
+                                            "IsNotDefinedExpression": {
+                                                "Event": "beta.xheaders.age"
+                                            }
+                                        }
+                                    ]
+                                },
+                                "actions": [
+                                    {
+                                        "Action": {
+                                            "action": "debug",
+                                            "action_args": {}
+                                        }
+                                    }
+                                ],
+                                "enabled": true
+                            }
+                        }
+                    ]
+            }
+            """;
+
+    @Test
+    public void isNotDefinedExpression() {
+        RulesExecutor rulesExecutor = RulesExecutorFactory.createFromJson(JSON_IS_NOT_DEFINED);
+
+        // A rule containing only a "isNotDefined" constraint matches only the first time ...
+        List<Match> matchedRules = rulesExecutor.processEvents("{\"meta\":{\"headers\":{\"token\":123}}}").join();
+        assertEquals(1, matchedRules.size());
+        assertEquals("r1", matchedRules.get(0).getRule().getName());
+        assertEquals(InitialFactImpl.class, matchedRules.get(0).getObjects().get(0).getClass());
+
+        // ... and then no more, to avoid overwhelming the system with firings for each and every event not having that field
+        matchedRules = rulesExecutor.processEvents("{\"beta\":{\"headers\":{\"age\":23}}}").join();
+        assertEquals(0, matchedRules.size());
+
+        // ... unless an event having that field is inserted
+        matchedRules = rulesExecutor.processEvents("{\"beta\":{\"xheaders\":{\"age\":23}}}").join();
+        assertEquals(0, matchedRules.size());
+
+        // ... and then that event is explicitly removed from the system or expires
+        matchedRules = rulesExecutor.processRetractMatchingFacts("{\"beta\":{\"xheaders\":{\"age\":23}}}", false).join();
+        assertEquals(1, matchedRules.size());
+        assertEquals("r1", matchedRules.get(0).getRule().getName());
+
+        rulesExecutor.dispose();
+    }
+
+    public static final String JSON_IS_DEFINED_AND_IS_NOT_DEFINED =
+            """
+            {
+                "rules": [
+                    {
+                        "Rule": {
+                            "name": "r1",
+                            "condition": {
+                                "AllCondition": [
+                                    {
+                                        "AndExpression": {
+                                            "lhs": {
+                                                "IsDefinedExpression": {
+                                                    "Event": "meta"
+                                                }
+                                            },
+                                            "rhs": {
+                                                "IsNotDefinedExpression": {
+                                                    "Event": "meta.headers.token"
+                                                }
+                                            }
+                                        }
+                                    }
+                                ]
+                            },
+                            "actions": [
+                                {
+                                    "Action": {
+                                        "action": "debug",
+                                        "action_args": {}
+                                    }
+                                }
+                            ],
+                            "enabled": true
+                        }
+                    }
+                ]
+            }
+            """;
+
+    @Test
+    public void isDefinedAndIsNotDefinedExpression() {
+        RulesExecutor rulesExecutor = RulesExecutorFactory.createFromJson(JSON_IS_DEFINED_AND_IS_NOT_DEFINED);
+
+        List<Match> matchedRules = rulesExecutor.processEvents("{\"meta\":{\"headers\":{\"token\":123}}}").join();
+        assertEquals(0, matchedRules.size());
+
+        matchedRules = rulesExecutor.processEvents("{\"meta\":{\"headers\":{\"age\":23}}}").join();
+        assertEquals(1, matchedRules.size());
+        assertEquals("r1", matchedRules.get(0).getRule().getName());
+
+        rulesExecutor.dispose();
+    }
+
+    public static final String JSON_IS_DEFINED_IS_NOT_DEFINED_IN_ALL =
+            """
+            {
+                "rules": [
+                    {
+                        "Rule": {
+                            "name": "r1",
+                            "condition": {
+                                "AllCondition": [
+                                    {
+                                        "IsDefinedExpression": {
+                                            "Event": "meta"
+                                        }
+                                    },
+                                    {
+                                        "IsNotDefinedExpression": {
+                                            "Event": "meta.headers.token"
+                                        }
+                                    }
+                                ]
+                            },
+                            "actions": [
+                                {
+                                    "Action": {
+                                        "action": "debug",
+                                        "action_args": {}
+                                    }
+                                }
+                            ],
+                            "enabled": true
+                        }
+                    }
+                ]
+            }
+            """;
+
+    @Test
+    public void isDefinedIsNotDefinedInAllExpression() {
+        RulesExecutor rulesExecutor = RulesExecutorFactory.createFromJson(JSON_IS_DEFINED_IS_NOT_DEFINED_IN_ALL);
+
+        List<Match> matchedRules = rulesExecutor.processEvents("{\"meta\":{\"headers\":{\"token\":123}}}").join();
+        assertEquals(0, matchedRules.size());
+
+        matchedRules = rulesExecutor.processEvents("{\"meta\":{\"headers\":{\"age\":23}}}").join();
+        assertEquals(1, matchedRules.size());
+        assertEquals("r1", matchedRules.get(0).getRule().getName());
+
+        rulesExecutor.dispose();
+    }}
