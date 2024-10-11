081fb1152703dc50d8c2872da58d95b7d631f928
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-10-11 15:15:19 +0900

    [DROOLS-7635] ansible-rulebook : Raise an error when a condition comp… (#117)
    * [DROOLS-7635] ansible-rulebook : Raise an error when a condition compares incompatible types
    - alpha index test, beta index test
    
    beta index test fix
    
    add negate test and any test
    
    * refactor
    
    * minor comment fix

--- drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/domain/constraints/NegationOperator.java
+++ drools-ansible-rulebook-integration-api/src/main/java/org/drools/ansible/rulebook/integration/api/domain/constraints/NegationOperator.java
@@ -4,6 +4,8 @@ import java.util.function.BiPredicate;
 
 import org.drools.model.ConstraintOperator;
 
+import static org.drools.ansible.rulebook.integration.api.domain.constraints.RulebookConstraintOperator.isCompatibleType;
+
 public class NegationOperator implements ConstraintOperator {
 
     private final ConstraintOperator toBeNegated;
@@ -14,6 +16,16 @@ public class NegationOperator implements ConstraintOperator {
 
     @Override
     public <T, V> BiPredicate<T, V> asPredicate() {
+        if (toBeNegated instanceof RulebookConstraintOperator rulebookConstraintOperator) {
+            return (t, v) -> {
+                // if not compatible type, return false. Use the operator to log the type check error
+                if (!isCompatibleType(t, v)) {
+                    rulebookConstraintOperator.logTypeCheck(t, v);
+                    return false;
+                }
+                return !toBeNegated.asPredicate().test(t, v);
+            };
+        }
         return (t, v) -> !toBeNegated.asPredicate().test(t, v);
     }
 
