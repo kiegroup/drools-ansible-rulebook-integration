80bcc530743b9dbc19f649664a1cf22b2f96e0d3
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2025-06-06 16:06:08 +0900

    add size parameter

--- drools-ansible-rulebook-integration-main/load_test.sh
+++ drools-ansible-rulebook-integration-main/load_test.sh
@@ -1,39 +1,40 @@
 #!/usr/bin/env bash
 
-# Usage: ./load_test.sh
-# This script runs a series of test files. While you can confirm STDOUT, the results are redirected to result.txt through STDERR.
-# The result would be "testfile name, usedMemory (bytes), timeTaken (ms)"
-# -------- for example --------
-# 24kb_1k_events.json, 5246824, 192
-# 24kb_10k_events.json, 5221216, 648
-# 24kb_100k_events.json, 5285568, 3958
-# 24kb_1m_events.json, 5282712, 33741
-# 24kb_1k_events_unmatch.json, 5158648, 122
-# 24kb_10k_events_unmatch.json, 5166952, 364
-# 24kb_100k_events_unmatch.json, 5249480, 1682
-# 24kb_1m_events_unmatch.json, 5291136, 12940
+# Usage: ./load_test.sh [1k|10k|100k|1m]
+#
+# Defaults to “1k” if you don’t pass any argument. It will run two test files:
+#   24kb_<size>_events.json
+#   24kb_<size>_events_unmatch.json
+# and append both results into result_<size>.txt
 
-# Truncate (or create) result.txt so we start fresh each time
-> result.txt
+# 1) Pick up the size argument, default to "1k"
+size="${1:-1k}"
 
-# List of JSON files to process
-files=(
-  "24kb_1k_events.json"
-  "24kb_10k_events.json"
-  "24kb_100k_events.json"
-  "24kb_1m_events.json"
-  "24kb_1k_events_unmatch.json"
-  "24kb_10k_events_unmatch.json"
-  "24kb_100k_events_unmatch.json"
-  "24kb_1m_events_unmatch.json"
-)
+# 2) Validate it
+case "$size" in
+  1k|10k|100k|1m) ;;
+  *)
+    echo "Invalid size: $size"
+    echo "Usage: $0 [1k|10k|100k|1m]"
+    exit 1
+    ;;
+esac
 
-# Loop over each file and run the java command
-for file in "${files[@]}"; do
-  echo "Running on ${file}..."
+# 3) Prepare filenames for “match” and “unmatch”
+match_file="24kb_${size}_events.json"
+unmatch_file="24kb_${size}_events_unmatch.json"
+
+# 4) Truncate (or create) a fresh result file
+out="result_${size}.txt"
+> "$out"
+
+# 5) Run both tests in sequence
+for file in "$match_file" "$unmatch_file"; do
+
+  echo "Running on $file..."
   java -Xmx512m -Dorg.slf4j.simpleLogger.logFile=System.out \
        -jar target/drools-ansible-rulebook-integration-main-jar-with-dependencies.jar \
-       "${file}" 2>> result.txt
+       "$file" 2>> "$out"
 done
 
-echo "All runs complete. See result.txt for any STDERR output (testfile, usedMemory, timeTaken)."
+echo "All runs complete for size=${size}. See $out for the STDERR output."
